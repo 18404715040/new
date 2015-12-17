f1d4f4cd7ac3233fadd6d18ae8a9d88614602e6c
20141105074 <20141105074qf@mac25.local>
2015-12-17 10:48:02 +0800

    Initial Commit

---
+++ new/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  new
+//
+//  Created by 20141105074 on 15/12/17.
+//  Copyright © 2015年 20141105074. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
